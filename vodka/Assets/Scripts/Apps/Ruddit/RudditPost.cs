89b03a5173a96cdd43260098265ff05a1f445aa3
Linden Reid <22460579+lindenreid@users.noreply.github.com>
2019-05-14 22:56:36 -0700

    v basic reddit UI
    list of posts that you can toggle open

---
+++ vodka/Assets/Scripts/Apps/Ruddit/RudditPost.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RudditPost : MonoBehaviour
+{
+    public Image ProfileImage;
+    public Text TitleText;
+    public Text UsernameText;
+    public GameObject PostContent;
+
+    public void ToggleOpenPost () {
+        PostContent.SetActive(!PostContent.activeSelf);
+    }
+}
